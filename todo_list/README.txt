2ab91232714a8b6836de164322626c09a642aecc
amykangweb <selene6023@gmail.com>
2015-04-10 20:07:55 -0700

    Initial Commit

---
+++ todo_list/README.txt
@@ -0,0 +1,3 @@
+Get Started with Sinatra by Tim Millwood
+
+http://www.creativebloq.com/web-design/get-started-sinatra-9134565
---
+++ todo_list/web.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'data_mapper'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/todo_list.db")
+  class Item
+    include DataMapper::Resource
+    property :id, Serial
+    property :content, Text, :require => true
+    property :done, Boolean, :required => true, :default => false
+    property :created, DateTime
+  end
+DataMapper.finalize.auto_upgrade!
+
+get '/' do
+  @items = Item.all(:order => :created.desc)
+  redirect '/new' if @items.empty?
+  erb :index
+end
+
+get '/new' do
+  @title = "Add todo item"
+  erb :new
+end
+
+post '/new' do
+  Item.create(:content => params[:content], :created => Time.now)
+  redirect '/'
+end
